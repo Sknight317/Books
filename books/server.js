93d190cf5f6d67b0a1ba6f55a59707fc7e30c84c
Sknight317 <Sknight23@mail.valenciacollege.edu>
2019-01-26 22:16:20 -0500

    fixed things

--- books/server.js
+++ books/server.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
-require('./models');
+// require('./models');
 
 const URI = require("./config/index");
 
