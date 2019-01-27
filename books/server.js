e428fefc339e1d82fa6697023296cf39f936b0f4
Sknight317 <Sknight23@mail.valenciacollege.edu>
2019-01-26 19:12:21 -0500

    new

--- books/server.js
+++ books/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-mongoose.plugin(schema => { schema.options.usePushEach = true });
+// mongoose.plugin(schema => { schema.options.usePushEach = true });
 const routes = require("./routes");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// require('./models');
+require('./models');
+
 const URI = require("./config/index");
 
 
